ef927363197c7e504e728f5d4d654c6ec3018cca
birthquake <rashiedtyre@gmail.com>
2026-03-27 15:57:05 -0400

    Update Halftime.jsx

--- src/pages/Halftime.jsx
+++ src/pages/Halftime.jsx
@@ -102,6 +102,7 @@ const Icon = {
       <line x1="21" y1="12" x2="9" y2="12"/>
     </svg>
   ),
+  StarFilled: () => (
     <svg width="14" height="14" viewBox="0 0 24 24" fill="currentColor" stroke="currentColor" strokeWidth="2">
       <polygon points="12 2 15.09 8.26 22 9.27 17 14.14 18.18 21.02 12 17.77 5.82 21.02 7 14.14 2 9.27 8.91 8.26 12 2"/>
     </svg>
